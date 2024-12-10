a9a17739595e6cd2b26c33879431ceeb23961ec4
infernerner <101186811+infernerner@users.noreply.github.com>
2024-12-10 10:54:21 +0100

    added rotation

--- Assets/_Mandelbrot/Materials/MandelbrotRenderShader.shader
+++ Assets/_Mandelbrot/Materials/MandelbrotRenderShader.shader
@@ -4,6 +4,7 @@ Shader "Mandelbrot/MandelbrotRenderShader"
     {
         _MainTex ("Texture", 2D) = "white" {}
         _Area ("Area", vector) = (0,0,4,4)
+        _Angle ("Angle", range(-3.1415,3.1415)) = 0 
     }
     SubShader
     {
@@ -39,11 +40,27 @@ Shader "Mandelbrot/MandelbrotRenderShader"
             }
 
             float4 _Area;
+            float _Angle;
             sampler2D _MainTex;
+
+            float2 rot(float2 p ,float2 pivot,float a)
+            {
+                float s = sin(a);
+                float c = cos(a);
+
+                p -= pivot;
+
+                p = float2(p.x*c-p.y*s,p.x*s+p.y*c);
+
+                p += pivot;
+
+                return p;
+            }
 
             fixed4 frag (v2f i) : SV_Target
             {
                 float2 c= _Area.xy + (i.uv-.5)*_Area.zw;
+                c = rot(c,_Area.xy,_Angle);
                 float2 z;
                 float iter;
                 for (iter = 0; iter < 255; iter++) {
--- Assets/_Mandelbrot/Scripts/Explorer.cs
+++ Assets/_Mandelbrot/Scripts/Explorer.cs
@@ -5,15 +5,18 @@ public class Explorer : MonoBehaviour
 {
     public Material mat;
     public Vector2 pos;
-    public float scale;
+    public float scale, angle;
 
     Vector2 smoothpos;
-    float smoothscale;
+    float smoothscale, smoothAngle;
     void UpdateShader()
     {
         smoothpos = Vector2.Lerp(smoothpos, pos, .1f);
         smoothscale = math.lerp(smoothscale, scale, .1f);
+        smoothAngle = math.lerp(smoothAngle, angle, .1f);
+
         float aspect = (float)Screen.width / (float)Screen.height;
+
         float scalex = smoothscale;
         float scaley = smoothscale;
 
@@ -22,6 +25,7 @@ public class Explorer : MonoBehaviour
         else
             scalex *= aspect;
         mat.SetVector("_Area", new Vector4(smoothpos.x, smoothpos.y,scalex,scaley));
+        mat.SetFloat("_Angle", smoothAngle);
 
     }
     void HandleInputs()
@@ -31,15 +35,28 @@ public class Explorer : MonoBehaviour
         if (Input.GetKey(KeyCode.KeypadMinus))
             scale *= 1.01f;
 
-        if (Input.GetKey(KeyCode.A))
-            pos.x -= .01f * scale;
-        if (Input.GetKey(KeyCode.D))
-            pos.x += .01f * scale;
+        if (Input.GetKey(KeyCode.E))
+            angle -= .01f;
+        if (Input.GetKey(KeyCode.Q))
+            angle += .01f;
 
+        Vector2 dir = new Vector2(.01f * scale, 0);
+        float s = math.sin(angle);
+        float c = math.cos(angle);
+        dir = new Vector2(dir.x*c,dir.x*s);
+
+        if (Input.GetKey(KeyCode.A))
+            pos -= dir;
+        if (Input.GetKey(KeyCode.D))
+            pos += dir;
+
+        dir = new Vector2(-dir.y, dir.x);
         if (Input.GetKey(KeyCode.S))
-            pos.y -= .01f * scale;
+            pos -= dir;
         if (Input.GetKey(KeyCode.W))
-            pos.y += .01f * scale;
+            pos += dir;
+
+
     }
     private void FixedUpdate()
     {
